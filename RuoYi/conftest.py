690091a386b7fe238050c1ceccfc93d4f9117987
yangjian <3127606562@qq.com>
2026-05-20 11:27:30 +0800

    添加20个用户的JSON文件和参数化登录测试

---
+++ RuoYi/conftest.py
@@ -0,0 +1,23 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+@pytest.fixture(scope="function")
+def driver():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--start-maximized")
+
+    # 指定 ChromeDriver 路径
+    service = Service(executable_path=r"D:\ChromeDriver\chromedriver-win64\chromedriver.exe")
+
+    driver = webdriver.Chrome(service=service, options=options)
+
+    yield driver
+    driver.quit()
+
+
+@pytest.fixture(scope="function")
+def wait(driver):
+    return WebDriverWait(driver, 10)
